1aa186562dd2a50d0f0f7e83cdee5ed5a3aba4ef
volkfm <volkfm98@gmail.com>
2021-07-19 17:04:47 +0500

    Add ClothingItem and many-to-many relation models

---
+++ WearMatcher/Models/ClothingItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WearMatcher.Models
+{
+    public class ClothingItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public List<MatchingItems> MatchingItems { get; set; }
+    }
+
+    public class MatchingItems
+    {
+        public int ItemId { get; set; }
+        public virtual ClothingItem Item { get; set; }
+
+        public int MatchingItemId { get; set; }
+        public virtual ClothingItem MatchingItem { get; set; }
+    }
+}
