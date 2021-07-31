a727e208b3b031db6285c8d30159a6f0c42cb1d2
volkfm <volkfm98@gmail.com>
2021-07-31 16:39:49 +0500

    Add TagsController

--- WearMatcher/Controllers/TagsController.cs
+++ WearMatcher/Controllers/TagsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WearMatcher.Data;
 using WearMatcher.Models;
+using WearMatcher.ViewModels.api;
 
 namespace WearMatcher.Controllers
 {
@@ -24,31 +25,45 @@ namespace WearMatcher.Controllers
         public async Task<IActionResult> Get()
         {
             List<Tag> tags = await _context.Tag.ToListAsync();
-            return new OkObjectResult(tags);
+
+            List<TagViewModel> viewModel = new();
+
+            foreach (var tag in tags)
+            {
+                viewModel.Add(new(tag));
+            }
+
+            return new OkObjectResult(viewModel);
         }
 
         [HttpGet("{id?}")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
-             Tag tag = await _context.Tag.FindAsync(id);
+             Tag tag =
+                await _context.Tag
+                .Include(t => t.ClothingItems)
+                .FirstOrDefaultAsync(t => t.Id == id);
 
             if (tag != null)
             {
-                return new OkObjectResult(tag);
+                TagFullViewModel viewModel = new(tag);
+                return new OkObjectResult(viewModel);
             }
 
             return NotFound();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromForm, Bind("Id, Name, ClothingItems")] Tag tag)
+        public async Task<IActionResult> Create([FromForm/*, Bind("Name, ClothingItemIds")*/] TagFullViewModel tagViewModel)
         {
+            Tag tag = await tagViewModel.Reflect(_context);
+
             if (ModelState.IsValid)
             {
                 await _context.Tag.AddAsync(tag);
                 await _context.SaveChangesAsync();
 
-                return CreatedAtAction("Get", tag.Id, tag);
+                return CreatedAtAction("Get", tag.Id, tagViewModel);
             }
 
             return BadRequest();
@@ -69,33 +84,23 @@ namespace WearMatcher.Controllers
             return NotFound();
         }
 
-        public async Task<IActionResult> Update([FromRoute] int id, [FromForm, Bind("Id, Name")] Tag tag)
+        public async Task<IActionResult> Update([FromForm] TagFullViewModel tagViewModel)
         {
-            if (!_context.Tag.Any(t => t.Id == tag.Id))
+            if (!_context.Tag.Any(t => t.Id == tagViewModel.Id))
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(tag);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!_context.Tag.Any(t => t.Id == tag.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                Tag tag = await tagViewModel.Reflect(_context);
+
+                _context.Update(tag);
+                await _context.SaveChangesAsync();
+                
                 return new OkObjectResult("Tag was successfully updated");
             }
+
             return BadRequest();
         }
     }
